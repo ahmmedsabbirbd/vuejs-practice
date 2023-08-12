530b499ccf41dbbf6c97d1942dc7f24dbf40f880
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-08-12 10:19:30 +0600

    add vuejs hook

---
+++ vue-hook/src/components/HelloWorld-child.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {onBeforeUnmount, onUnmounted} from "vue";
+
+
+// #5 before unmounted
+onBeforeUnmount(()=> {
+    console.log('(hellow child) before unmounted onBeforeUnmount()')
+})
+
+// #6 when unmounted
+onUnmounted(()=> {
+    console.log('(hellow child) when unmounted onUnmounted()')
+})
+
+</script>
+
+<template>
+    <div>
+        <h2>this is hellow child</h2>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-hook/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref, onMounted, onUpdated, onBeforeUpdate} from "vue";
+import HelloWorldChild from "./HelloWorld-child.vue";
+
+const count_child = ref(0);
+const show = ref(false)
+
+// after create DOM tree
+onMounted(()=> {
+    console.log('this is child')
+});
+
+// when DOM tree change
+onUpdated(()=> {
+    console.log('updated hellow')
+    console.log(document.getElementById('count_child').textContent)
+})
+
+
+</script>
+
+<template>
+    <div>
+        <h2>this is child</h2>
+    </div>
+
+    <button id="count_child" @click="count_child++">{{ count_child }}</button>
+
+    <br>
+    <button @click="show=true">Show HelloWorldChild</button>
+    <button @click="show=false">hide HelloWorldChild</button>
+
+    <HelloWorldChild v-if="show"/>
+</template>
+
+<style scoped>
+
+</style>
